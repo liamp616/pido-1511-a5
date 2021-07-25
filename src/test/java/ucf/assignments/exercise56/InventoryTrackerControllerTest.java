fe6aff066c2fdfbc77b18465dd188afa80c76e46
Liam <xlime1p@gmail.com>
2021-07-25 16:36:02 -0400

    Initial Solution (addItem, removeItem, editItem, search, prevents similar sn, export/load tsv, export/load json, export html, uml diagram)

---
+++ src/test/java/ucf/assignments/exercise56/InventoryTrackerControllerTest.java
@@ -0,0 +1,51 @@
+package ucf.assignments.exercise56;
+
+import org.junit.jupiter.api.Test;
+
+public class InventoryTrackerControllerTest {
+
+    @Test
+    void addItemClicked() {
+
+    }
+
+    @Test
+    void removeItemClicked() {
+
+    }
+
+    @Test
+    void editItemClicked() {
+
+    }
+
+    @Test
+    void exportHTMLClicked() {
+
+    }
+
+    @Test
+    void importHTMLClicked() {
+
+    }
+
+    @Test
+    void exportJSONClicked() {
+
+    }
+
+    @Test
+    void importJSONClicked() {
+
+    }
+
+    @Test
+    void importTSVClicked() {
+
+    }
+
+    @Test
+    void exportTSVClicked() {
+
+    }
+}
